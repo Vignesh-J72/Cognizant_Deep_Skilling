7f8ecea1d0b01d27d6d5e155fe7d2a0d445e8067
Vignesh <vigneshj722@gmail.com>
2026-06-23 20:53:41 +0530

    Add pytest syntax practice
    Added a program used for practicing basic pytest functions and methods.

---
+++ Module4_UnitTesting/PyTest/test_syntax.py
@@ -0,0 +1,27 @@
+import pytest
+
+#Assert using function
+def func(x):
+    return x+5
+
+#Multiple tests in one class
+class TestClass:
+    def test_one(self):
+        x=0
+        assert x==1
+    
+    def test_two(self):
+        x="H"
+        assert x in "Hello world"
+
+
+
+if __name__=="__main__":
+    #Basic assert
+    assert sum([12,3,5])==20, "Should be equal to 20"
+    
+    #assert using function
+    assert func(4)==9
+
+    
+    
