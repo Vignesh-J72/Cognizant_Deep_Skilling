dd60bb5f783eb7448313c8f470d1bb36662cba18
Vignesh <vigneshj722@gmail.com>
2026-07-22 22:57:46 +0530

    Add hands on 9 folder

---
+++ Module6_FrontendDevelopmentBasics/HandsOn_9/data.js
@@ -0,0 +1,7 @@
+export const courses=[
+    {id:1, name:'Web Development Foundations', code:'CS101', credits:3, grade:'A'},
+    {id:2, name:'Data Structures & Algorithms', code:'CS102', credits:4, grade:'A-'},
+    {id:3, name:'Database Management Systems', code:'CS201', credits:3, grade:'B+'},
+    {id:4, name:'Software Engineering Principles', code:'CS301', credits:3, grade:'A'},
+    {id:5, name:'Computer Networks', code:'CS401', credits:4, grade:'B'}
+];
